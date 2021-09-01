25189344fe65a4a48ee2bfda2377409bb9b3f7b6
Szabus <szendroi.szabina@gmail.com>
2021-09-01 19:20:57 +0200

    add odd number counter

--- src/Practice/Main.java
+++ src/Practice/Main.java
@@ -34,9 +34,20 @@ public class Main {
         }return min;
     }
 
+    public static int oddNums(int[] randInts){
+        int counter = 0;
+
+        for(int i = 0; i <randInts.length; i++){
+            if (randInts[i] % 2 != 0){
+                counter++;
+            }
+        }return counter;
+    }
+
     public static void main(String[] args) {
 
         System.out.println(Arrays.toString(randInts()));
         System.out.println(randMax(randInts()));
+        System.out.println(oddNums(randInts()));
     }
 }
