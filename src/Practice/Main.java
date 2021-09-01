8752b51fb820439d0e434babc1b2c2e0ee207fec
sumflo <real61madrid@gmail.com>
2021-09-01 18:54:35 +0200

    array

--- src/Practice/Main.java
+++ src/Practice/Main.java
@@ -1,8 +1,22 @@
 package Practice;
 
+import java.util.Arrays;
+
 public class Main {
 
+    public static int[] randInts(){
+        int[] randInts = new int[20];
+
+        for (int i = 0; i < randInts.length; i++) {
+            randInts[i] = (int) ((Math.random() * (101 - 1)) + 1);
+        }
+
+        return randInts;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println(Arrays.toString(randInts()));
+
     }
 }
